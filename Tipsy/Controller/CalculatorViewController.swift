3354b31f43ed0d4c614116da868ab8ff5ddd2bb1
Favour <gh.oghene@gmail.com>
2025-09-08 09:28:02 +0100

    calculated tip amount

--- Tipsy/Controller/CalculatorViewController.swift
+++ Tipsy/Controller/CalculatorViewController.swift
@@ -12,12 +12,15 @@ class CalculatorViewController: UIViewController {
     
     var tipAmount = 0.1
     var splitNumber = 2.0
+    var totalBill: Double = 0
+ 
 
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
+    
 
     var tipButtons: [UIButton] {
         return [zeroPctButton, tenPctButton, twentyPctButton]
@@ -26,6 +29,7 @@ class CalculatorViewController: UIViewController {
     let tips:[String: Double] = ["0%": 0, "10%": 0.1, "20%": 0.2]
     
     @IBAction func tipChanged(_ sender: UIButton) {
+        billTextField.endEditing(true)
         
         for button in tipButtons {
             button.isSelected = false
@@ -39,12 +43,19 @@ class CalculatorViewController: UIViewController {
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
         splitNumberLabel.text = String(Int(sender.value))
         splitNumber = sender.value
-        
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
         print(tipAmount)
         print(splitNumber)
+        
+        print(billTextField.text!)
+        totalBill = Double(billTextField.text ?? "0") ?? 0
+        
+        
+        let billForEachPerson = (totalBill + (totalBill * tipAmount)) / splitNumber
+        
+        print(String(format: "%.2f", billForEachPerson))
     }
 }
 
